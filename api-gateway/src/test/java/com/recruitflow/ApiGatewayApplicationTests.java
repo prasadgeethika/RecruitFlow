733a1b5a0a2471a62e3fd0c3984e555d8eb01436
Prasad Geethika <prasad.geethika@globallogic.com>
2026-08-01 21:26:32 +0530

    feat(gateway): add API Gateway with route to auth-service

---
+++ api-gateway/src/test/java/com/recruitflow/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
